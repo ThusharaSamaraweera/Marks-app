8cc09f2b1c281a8086d3cd9cc3fb50268c095dc4
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-08-21 00:16:22 +0530

    changed color of mark to green

--- src/Components/AddBoard.tsx
+++ src/Components/AddBoard.tsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import {Button, Col, Form, Row} from "react-bootstrap";
 
-const AddBoard:React.FC = () => {
+type AddBoardProps = {
+  onTeamAdd:(name:string) => void;
+}
+const AddBoard:React.FC<AddBoardProps> = (props) => {
+  const [newTeamName, setNewTeamName] = useState<string | null>(null);
+
+  const handleOnSubmit = (event:any) => {
+    event.preventDefault();
+    if(!newTeamName || newTeamName === ''){
+      return;
+    }
+    props.onTeamAdd(newTeamName);
+    setNewTeamName(null)
+  }
+
+  const handleOnTeamNameChanged = (name: string) => {
+    setNewTeamName(name);
+  }
+
   return (
       <Col xs={12} sm={5} className='add-board mt-2'>
         <Row xs={12} className='pl-3'>
           <h2>Add Board</h2>
         </Row>
         <Row xs={12} className='from-row px-3'>
-          <Form className='formInputs py-3'
+          <Form className='formInputs py-3' onSubmit={handleOnSubmit}
           >
             <Form.Group controlId="teamName">
               <Form.Label>Name of Team</Form.Label>
-              <Form.Control type="text" placeholder=""
+              <Form.Control type="text" placeholder="Enter the team name"
                             required
+                            value={newTeamName ? newTeamName : ''}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                              handleOnTeamNameChanged(event.target.value)}
               />
               </Form.Group>
-            <Button className='add-btn float-right mb-3'>ADD</Button>
+            <Button className='add-btn float-right mb-3'
+                    type='submit'
+            >ADD</Button>
           </Form>
         </Row>
       </Col>
--- src/Marks-app.tsx
+++ src/Marks-app.tsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import {Container, Row} from "react-bootstrap";
 import Title from "../src/Components/Title";
 import AddBoard from "./Components/AddBoard";
 import CreatedTeams from "./Components/CreatedTeams";
+import {ITeam} from "./Types/MarksTypes";
 
-const MarksApp:React.FC = () => {
+const MarksApp: React.FC = () => {
+  const allTeams:ITeam[] = [
+    {id: 1, name: "team1", marks: 100},
+    {id: 2, name: "team2", marks: 200},
+    {id: 3, name: "team3", marks: 400},
+    {id: 4, name: "team4", marks: 400}
+  ]
+
+  const [teams, setTeams] = useState<ITeam[]>(allTeams);
+
+  const generateId = () => {
+    return Math.floor(Math.random()*100 + 1);
+  }
+
+  const handleOnAddedTeam = (teamName:string) => {
+    const tempAllTeams:ITeam[] = allTeams.slice();
+    const Ids: number[] = teams.map(team => team.id)
+    let id: number = 0;
+    do{
+      id = generateId();
+    }while(!Ids.includes(id));
+
+    const newTeam:ITeam = {id:id, name:teamName, marks:0};
+    tempAllTeams.push(newTeam);
+    setTeams(tempAllTeams)
+  }
+
+  const handleOnDelete = (id:number) => {
+    const confirmation:boolean = window.confirm("Delete Team?");
+    if(confirmation){
+      const tempAllTeams:ITeam[] = allTeams.slice();
+      setTeams(
+        tempAllTeams.filter( (team) => 
+          team.id !== id
+        )
+      );
+    }
+  }
 
   return (
     <Container fluid>
       <Title/>
       <Row xs={12} md={7} className='mt-2 mx-0'>
-        <AddBoard/>
+        <AddBoard onTeamAdd={handleOnAddedTeam}/>
       </Row>
-      <CreatedTeams/>
+      <CreatedTeams teams={teams} setTeams={setTeams}
+              onDeleteTeam={handleOnDelete}
+      />
 
     </Container>
   )
